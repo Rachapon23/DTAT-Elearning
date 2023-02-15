835923051e6047940dab93c352a7ad682db0ccc8
DIAT-WSC <ohm32070@gmail.com>
2023-02-15 09:40:27 +0700

    resloved conflict

--- client/src/component/layout/NavStudent.js
+++ client/src/component/layout/NavStudent.js
@@ -30,43 +30,24 @@ const NavStudent = () => {
 
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-<<<<<<< HEAD
-          {/* <Nav.Link className="text-white" href="/student/home">หน้าแรก</Nav.Link> */}
-            {/* <Nav.Link className="text-white" href="/student/list-courses">คอร์สของฉัน</Nav.Link> */}
-=======
             {/* <Nav.Link className="text-white" href="/student/home">หน้าแรก</Nav.Link> */}
             <Nav.Link className="text-white" href="/student/list-courses">คอร์สของฉัน</Nav.Link>
->>>>>>> 1556c0df6a78f5f6bfa63a33b8bbe3e75b77d216
             {/* <Nav.Link href="/student/get-course/:id">/student/get-course/:id</Nav.Link> */}
           </Nav>
         </Navbar.Collapse>
 
         <Navbar.Collapse className="justify-content-end">
-<<<<<<< HEAD
-          <Navbar.Text
-          className="text-white">
-            Signed in {sessionStorage.getItem("role")} as: 
-          </Navbar.Text>
-            <NavDropdown  title={sessionStorage.getItem("firstname")} id="navbarScrollingDropdown">
-              <NavDropdown.Item onClick={logout}>logout</NavDropdown.Item>
-            </NavDropdown>
-=======
+
           <Navbar.Text>
             Signed in {sessionStorage.getItem("role")} as:
           </Navbar.Text>
           <NavDropdown title={sessionStorage.getItem("firstname")} id="navbarScrollingDropdown">
             <NavDropdown.Item onClick={logout}>logout</NavDropdown.Item>
           </NavDropdown>
->>>>>>> 1556c0df6a78f5f6bfa63a33b8bbe3e75b77d216
+
           {/* <Nav.Link onClick={logout}>logout</Nav.Link> */}
         </Navbar.Collapse>
-<<<<<<< HEAD
-      {/* </Container> */}
-=======
-
       </Container>
-
->>>>>>> 1556c0df6a78f5f6bfa63a33b8bbe3e75b77d216
     </Navbar>
   );
 };
