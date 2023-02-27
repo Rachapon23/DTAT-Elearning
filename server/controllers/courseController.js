d0f219ae24dac95a69e6297ce218cada8db86e71
DIAT WSC <patapee475@gmail.com>
2023-02-27 13:57:49 +0700

    for pull new code

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -133,7 +133,8 @@ exports.getCourse = async (req, res) => {
         res.send(course)
     }
     catch (err) {
-
+        console.log("fail to get courses");
+        res.status(500).json({ error: "fail to get courses" });
     }
 }
 
